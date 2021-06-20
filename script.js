a1dec98f016db99f3470d6cd47091dc2a0f22cf3
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-06-20 16:38:26 -0300

    minor fixes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const range = document.getElementById('myRange');
 const pageviews = document.getElementById('pageviews');
-const price = document.getElementById('price')
+const price = document.getElementById('price');
+const discount = document.getElementById('flexSwitchCheckDefault');
+let priceChange = 16;
 
 range.addEventListener('change', function(e){
-   console.log(range.value)
    pageviews.innerText = `${range.value*2}K `;
-   price.innerText = `${Math.ceil(range.value/4+3)}`
+   priceChange = `${Math.ceil(range.value/4+3)}`;
+   price.innerText = priceChange;
 })
 
+discount.addEventListener('checked', function(e){
+   console.log(priceChange)
+   if (discount === 'checked') {
+   }
+   // priceChange = priceChange - (priceChange*100)/25
+   // price.innerText = priceChange;
+})
 
