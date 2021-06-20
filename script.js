69dd822bb09e8a4615b012f5f0c0e90ca8877ed5
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-06-20 18:03:22 -0300

    Js finished

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const range = document.getElementById('myRange');
 const pageviews = document.getElementById('pageviews');
 const price = document.getElementById('price');
-const discount = document.getElementById('flexSwitchCheckDefault');
+const discountSwitch = document.getElementById('flexSwitchCheckDefault');
+const btn = document.querySelector("button");
 let priceChange = 16;
+let discount = 0;
+let priceDiscount = priceChange - discount;
+let count = 1;
 
-range.addEventListener('change', function(e){
+function changePrice(){
    pageviews.innerText = `${range.value*2}K `;
    priceChange = `${Math.ceil(range.value/4+3)}`;
-   price.innerText = priceChange;
+   let priceDiscount = priceChange - discount;
+   price.innerText = priceDiscount;
+}
+
+function discountF(){
+   count += 1;
+   if (count%2===0) {
+      discount = (priceChange*25)/100;
+      changePrice();
+   } else {
+      discount = 0;
+      changePrice();
+   }
+}
+
+function sayHello(){
+   console.log("Hola mundo")
+}
+
+range.addEventListener('change', (e) =>{
+   changePrice();
+   discountF();
+   discountF();
+});
+
+discountSwitch.addEventListener('change', (e) => {
+   discountF();
+})
+btn.addEventListener('click', (e) =>{
+   alert('Gracias por probar la página!')
 })
 
-discount.addEventListener('checked', function(e){
-   console.log(priceChange)
-   if (discount === 'checked') {
-   }
-   // priceChange = priceChange - (priceChange*100)/25
-   // price.innerText = priceChange;
-})
 
