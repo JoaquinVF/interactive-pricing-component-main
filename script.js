91909c2cbfc927daad148a517764a5ffa3325bb6
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-06-20 16:12:34 -0300

    js added

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-var input = document.querySelector("input[type=range]");
-   actualizarInput(input) 
+const range = document.getElementById('myRange');
+const pageviews = document.getElementById('pageviews');
+const price = document.getElementById('price')
+
+range.addEventListener('change', function(e){
+   console.log(range.value)
+   pageviews.innerText = `${range.value*2}K `;
+   price.innerText = `${Math.ceil(range.value/4+3)}`
+})
 
 
-input.addEventListener("input", function(evt) {
-   actualizarInput(input)
-});
-
-function actualizarInput(input){
-   var label = input.parentElement.querySelector("label");
-   label.innerHTML = input.value;
-   var inputMin = input.getAttribute("min");
-   var inputMax = input.getAttribute("max");
-   var unidad = (inputMax - inputMin) / 100;
-   input.style.setProperty("--value", (input.value - inputMin)/unidad);  
-}
